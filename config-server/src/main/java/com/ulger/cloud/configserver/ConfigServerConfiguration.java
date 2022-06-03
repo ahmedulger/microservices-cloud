2eb39ee9f68728ecb3d57815c424271354def1e7
ahulger <ahulger@ebay.com>
2022-06-03 22:19:56 +0300

    Added config signs to files

--- config-server/src/main/java/com/ulger/cloud/configserver/ConfigServerConfiguration.java
+++ config-server/src/main/java/com/ulger/cloud/configserver/ConfigServerConfiguration.java
@@ -1,7 +1,9 @@
 package com.ulger.cloud.configserver;
 
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableConfigServer
 public class ConfigServerConfiguration {
 
