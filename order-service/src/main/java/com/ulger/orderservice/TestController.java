310e990c04d489dff79627aa7c2e6b41921f8263
ahulger <ahulger@ebay.com>
2022-06-03 22:37:03 +0300

    asd

--- order-service/src/main/java/com/ulger/orderservice/TestController.java
+++ order-service/src/main/java/com/ulger/orderservice/TestController.java
@@ -1,5 +1,6 @@
 package com.ulger.orderservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private ProductServiceFeignClient productServiceFeignClient;
+
+    public TestController(ProductServiceFeignClient productServiceFeignClient) {
+        this.productServiceFeignClient = productServiceFeignClient;
+    }
+
     @Value("${test.greeting}")
     private String test;
 
     @RequestMapping("/greeting")
     public String greeting() {
-        return test;
+        return test + " - " + productServiceFeignClient.greeting();
     }
 }
