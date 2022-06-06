5f70d01a67c9e1c9f179626f78dd736e94d89032
ahmedulger <ahmedulger@gmail.com>
2022-06-06 21:03:45 +0300

    Development (#3)
    * Authentication server added
    
    * Gathered modules under a root project
    
    * Gathered modules under a root project
    
    * Gathered modules under a root project
    
    * Gathered modules under a root project
    
    * Gathered modules under a root project
    
    * Added oauth2 support
    
    * Added JWT support
    
    * Added JWT support
    
    Co-authored-by: ahulger <ahulger@ebay.com>

---
+++ authentication-server/src/main/java/com/ulger/cloud/authenticationserver/configuration/OAuth2AuthorizationServerConfig.java
@@ -0,0 +1,61 @@
+package com.ulger.cloud.authenticationserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenStore tokenStore;
+    private final PasswordEncoder passwordEncoder;
+    private final AccessTokenConverter accessTokenConverter;
+
+    public OAuth2AuthorizationServerConfig(
+            AuthenticationManager authenticationManager,
+            TokenStore tokenStore,
+            PasswordEncoder passwordEncoder,
+            AccessTokenConverter accessTokenConverter) {
+
+        this.authenticationManager = authenticationManager;
+        this.tokenStore = tokenStore;
+        this.passwordEncoder = passwordEncoder;
+        this.accessTokenConverter = accessTokenConverter;
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter)
+                .authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("order-service")
+                .secret(passwordEncoder.encode("pwd-order-service"))
+                .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(200000)
+                .refreshTokenValiditySeconds(200000);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }
+}
---
+++ authentication-server/src/main/java/com/ulger/cloud/authenticationserver/configuration/OAuth2ResourceServerConfig.java
@@ -0,0 +1,58 @@
+package com.ulger.cloud.authenticationserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableResourceServer
+public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final TokenStore tokenStore;
+    private final AuthenticationSuccessHandler authenticationSuccessHandler;
+    private final ResourceServerTokenServices resourceServerTokenServices;
+
+    public OAuth2ResourceServerConfig(
+            TokenStore tokenStore,
+            AuthenticationSuccessHandler authenticationSuccessHandler,
+            ResourceServerTokenServices resourceServerTokenServices) {
+        this.tokenStore = tokenStore;
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.resourceServerTokenServices = resourceServerTokenServices;
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                    .disable()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/register").permitAll()
+                    .antMatchers(HttpMethod.GET, "/register").permitAll()
+                    .antMatchers(HttpMethod.GET, "/message").access("#oauth2.hasScope('read')")
+                    .antMatchers(HttpMethod.GET, "/rest/api/v1/user/**").authenticated()
+                    .antMatchers(HttpMethod.POST, "/rest/api/v1/user").not().authenticated()
+                    .antMatchers(HttpMethod.PUT, "/rest/api/v1/user").authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .successHandler(authenticationSuccessHandler)
+                    .loginProcessingUrl("/perform_login").permitAll()
+                    .and()
+                .logout()
+                    .disable()
+                .sessionManagement()
+                    .disable();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.tokenStore(tokenStore).tokenServices(resourceServerTokenServices);
+    }
+}
